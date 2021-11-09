ecb54689876135abd524689a2f353e4810073443
Alexander Palm <alexander@alpa.se>
2021-11-09 10:51:33 +0100

    check if destination file already exists before compress

--- compress.sh
+++ compress.sh
@@ -10,23 +10,29 @@ DESTINATION=$2
 
 mkdir -p $DESTINATION
 
-for FILE in $FILES/*
+for FILE in $FILES\/*
 do
 if [[ "$FILE" == *\.* ]]
 then
-	WIDTH=`identify -format '%w' $FILE`
-	
-	QUALITY=85
-	
-	if [ $WIDTH > 3839 ]; then	
-		QUALITY=45
-	elif [ $WIDTH > 1959 ]; then	
-		QUALITY=55
-	elif [ $WIDTH > 1279 ]; then
-		QUALITY=65
-	fi
+	DESTINATION_FILE=$DESTINATION/$(basename "${FILE}")
 
-	convert $FILE -sampling-factor 4:2:0 -strip -quality $QUALITY -interlace JPEG -set colorspace RGB $DESTINATION/$(basename "${FILE}")
-	echo compressed $FILE "("$WIDTH"px, "$QUALITY"%)" $?
+	if [ -f "$DESTINATION_FILE" ]; then
+		echo "exists: $DESTINATION_FILE"
+	else
+		WIDTH=`identify -format '%w' "$FILE"`
+		
+		QUALITY=85
+		
+		if [ $WIDTH > 3839 ]; then	
+			QUALITY=45
+		elif [ $WIDTH > 1959 ]; then	
+			QUALITY=55
+		elif [ $WIDTH > 1279 ]; then
+			QUALITY=65
+		fi
+
+		convert "$FILE" -sampling-factor 4:2:0 -strip -quality $QUALITY -interlace JPEG -colorspace sRGB "$DESTINATION_FILE"
+		echo compressed $FILE "("$WIDTH"px, "$QUALITY"%)" $?
+	fi
 fi
 done
